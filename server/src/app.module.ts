e45515d45ca39adc25cd8d4b35dae84eeb403941
Илья Корнев <ilyakornev@MacBook-Pro-Ila.local>
2021-07-21 11:57:03 +0300

    added enviroment, create var port

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 @Module({
    controllers: [],
    providers: [],
    imports: [
+      ConfigModule.forRoot({ envFilePath: `.${process.env.NODE_ENV}.env` }),
       SequelizeModule.forRoot({
          dialect: 'postgres',
-         host: 'localhost',
-         port: 5543,
-         username: 'admin',
-         password: '123123',
-         database: 'vadjiShop',
-         models: []
-         })
-  ]
+         host: process.env.POSTGRES_HOST,
+         port: Number(process.env.POSTGRES_PORT),
+         username: process.env.POSTGRES_USERNAME,
+         password: process.env.POSTGRES_PASSWORD,
+         database: process.env.POSTGRES_DB,
+         models: [],
+         autoLoadModels: true
+      })
+   ]
 })
 export class AppModule { }
